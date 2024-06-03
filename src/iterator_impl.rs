8538a6714e2ab539677b527b1b56d3377c5b53b1
GnomedDev <david2005thomas@gmail.com>
2024-06-03 16:54:41 +0100

    Conform to clippy::pedantic and rust_2018_idioms

--- src/iterator_impl.rs
+++ src/iterator_impl.rs
@@ -4,19 +4,29 @@ use crate::OptArrayVec;
 
 pub struct Iter<'a, T, const CAP: usize> {
 	parent: &'a OptArrayVec<T, CAP>,
-	index:  usize,
-	len:    usize,
+	index: usize,
+	len: usize,
 }
 
 impl<T, const CAP: usize> OptArrayVec<T, CAP> {
-	pub fn iter(&self) -> Iter<T, CAP> {
+	pub fn iter(&self) -> Iter<'_, T, CAP> {
 		Iter {
 			parent: self,
-			index:  0,
-			len:    self.len(),
+			index: 0,
+			len: self.len(),
 		}
 	}
 }
+
+impl<'a, T, const CAP: usize> IntoIterator for &'a OptArrayVec<T, CAP> {
+	type Item = &'a T;
+	type IntoIter = Iter<'a, T, CAP>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		self.iter()
+	}
+}
+
 impl<'a, T, const CAP: usize> Iterator for Iter<'a, T, CAP> {
 	type Item = &'a T;
 
