bbf6954b5b92046ae20f2863639c397d5e357b60
FlareFlo <mail@flareflo.dev>
2024-06-04 00:17:34 +0200

    Missing comment

--- src/impls.rs
+++ src/impls.rs
@@ -4,6 +4,7 @@ impl<T, const CAP: usize> OptArrayVec<T, CAP> {
 	// Binds T, therefore must be in the global context, meaning it cannot be moved into Self::new
 	const ARRAY_REPEAT_VALUE: Option<T> = None;
 
+	/// Returns capacity
 	pub fn cap(&self) -> usize {
 		CAP
 	}
