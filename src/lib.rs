822f91bf97f7aa1c8fe588f010a58174718f3f36
FlareFlo <mail@flareflo.dev>
2024-06-03 15:56:54 +0200

    Fix comment visibility issue

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod tests;
 mod trait_impl;
 pub mod iterator_impl;
 
-// A vector-like container that does not store its length directly, but rather retrieves them through inner options
+/// A vector-like container that does not store its length directly, but rather retrieves them through inner options
 #[derive(Debug, Copy, Clone)]
 pub struct OptArrayVec<const N: usize, T> {
 	inner: [Option<T>; N],
