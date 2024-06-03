d0c9602585e6de5e70b23204dbc873ec42d6d92a
GnomedDev <david2005thomas@gmail.com>
2024-06-03 16:46:13 +0100

    Simplify Iter/IntoIter implementations

--- src/iterator_impl.rs
+++ src/iterator_impl.rs
@@ -1,42 +1,33 @@
-use std::ops::Index;
-
 use crate::OptArrayVec;
 
-pub struct Iter<'a, T, const CAP: usize> {
-	parent: &'a OptArrayVec<T, CAP>,
-	index: usize,
-	len: usize,
+pub struct Iter<'a, T> {
+	inner: std::slice::Iter<'a, Option<T>>,
 }
 
 impl<T, const CAP: usize> OptArrayVec<T, CAP> {
-	pub fn iter(&self) -> Iter<'_, T, CAP> {
+	pub fn iter(&self) -> Iter<'_, T> {
 		Iter {
-			parent: self,
-			index: 0,
-			len: self.len(),
+			inner: self.inner.iter(),
 		}
 	}
 }
 
 impl<'a, T, const CAP: usize> IntoIterator for &'a OptArrayVec<T, CAP> {
 	type Item = &'a T;
-	type IntoIter = Iter<'a, T, CAP>;
+	type IntoIter = Iter<'a, T>;
 
 	fn into_iter(self) -> Self::IntoIter {
 		self.iter()
 	}
 }
 
-impl<'a, T, const CAP: usize> Iterator for Iter<'a, T, CAP> {
+impl<'a, T> Iterator for Iter<'a, T> {
 	type Item = &'a T;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		if self.index < self.len {
-			let ret = self.parent.index(self.index);
-			self.index += 1;
-			Some(ret)
-		} else {
-			None
+		match self.inner.next() {
+			Some(Some(item)) => Some(item),
+			Some(None) | None => None,
 		}
 	}
 }
